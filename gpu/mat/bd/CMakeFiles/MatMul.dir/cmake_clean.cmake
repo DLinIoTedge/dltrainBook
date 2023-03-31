e6ad99c9ba550030bde44662e91255d3f9c8013f
jk <58679469+DLinIoTedge@users.noreply.github.com>
2023-03-31 23:31:50 +0530

    Add files via upload
    matrix multiplication

---
+++ gpu/mat/bd/CMakeFiles/MatMul.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MatMul.dir/matsrc/jkernel.cu.o"
+  "CMakeFiles/MatMul.dir/matsrc/jmatMul.cu.o"
+  "CMakeFiles/MatMul.dir/cmake_device_link.o"
+  "../bin/MatMul.pdb"
+  "../bin/MatMul"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/MatMul.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gpu/mat/matsrc/jmatMul.cu
@@ -0,0 +1,111 @@
+
+// https://www.quantstart.com/articles/Matrix-Matrix-Multiplication-on-the-GPU-with-Nvidia-CUDA/
+// May 23 2020 //
+
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+#include <cuda_runtime.h>
+#include "jkernel.h"
+#include "jmatArray.h"
+#include <math.h>
+
+using namespace std;
+
+#if 0
+__global__ void matrixMultiplicationKernel(float* A, float* B, float* C, int N) {
+
+    int ROW = blockIdx.y*blockDim.y+threadIdx.y;
+    int COL = blockIdx.x*blockDim.x+threadIdx.x;
+
+    float tmpSum = 0;
+
+    if (ROW < N && COL < N) {
+        // each thread computes one element of the block sub-matrix
+        for (int i = 0; i < N; i++) {
+            tmpSum += A[ROW * N + i] * B[i * N + COL];
+        }
+    }
+    C[ROW * N + COL] = tmpSum;
+}
+
+
+void matrixMultiplication(float *A, float *B, float *C, int N){
+
+    // declare the number of blocks per grid and the number of threads per block
+    // use 1 to 512 threads per block
+    dim3 threadsPerBlock(N, N);
+    dim3 blocksPerGrid(1, 1);
+        if (N*N > 512){
+            threadsPerBlock.x = 512;
+            threadsPerBlock.y = 512;
+            blocksPerGrid.x = ceil(double(N)/double(threadsPerBlock.x));
+            blocksPerGrid.y = ceil(double(N)/double(threadsPerBlock.y));
+        }
+
+    matrixMultiplicationKernel<<<blocksPerGrid,threadsPerBlock>>>(A, B, C, N);
+}
+#endif
+
+int main()
+{
+    // Perform matrix multiplication C = A*B
+    // where A, B and C are NxN matrices
+    int N = 16;
+    int SIZE = N*N;
+
+    // Allocate memory on the host
+    vector<float> h_A(SIZE);
+    vector<float> h_B(SIZE);
+    vector<float> h_C(SIZE);
+
+    // Initialize matrices on the host
+    for (int i=0; i<N; i++){
+        for (int j=0; j<N; j++){
+            h_A[i*N+j] = sin(i);
+            h_B[i*N+j] = cos(j);
+        }
+    }
+
+    // Allocate memory on the device
+    dev_array<float> d_A(SIZE);
+    dev_array<float> d_B(SIZE);
+    dev_array<float> d_C(SIZE);
+
+    d_A.set(&h_A[0], SIZE);
+    d_B.set(&h_B[0], SIZE);
+
+    matrixMultiplication(d_A.getData(), d_B.getData(), d_C.getData(), N);
+    cudaDeviceSynchronize();
+
+    d_C.get(&h_C[0], SIZE);
+    cudaDeviceSynchronize();
+
+    float *cpu_C;
+    cpu_C=new float[SIZE];
+
+    // Now do the matrix multiplication on the CPU
+    float sum;
+    for (int row=0; row<N; row++){
+        for (int col=0; col<N; col++){
+            sum = 0.f;
+            for (int n=0; n<N; n++){
+                sum += h_A[row*N+n]*h_B[n*N+col];
+            }
+            cpu_C[row*N+col] = sum;
+        }
+    }
+
+    double err = 0;
+    // Check the result and make sure it is correct
+    for (int ROW=0; ROW < N; ROW++){
+        for (int COL=0; COL < N; COL++){
+            err += cpu_C[ROW * N + COL] - h_C[ROW * N + COL];
+        }
+    }
+
+    cout << "jk Error: " << err << endl;
+
+    return 0;
+}
